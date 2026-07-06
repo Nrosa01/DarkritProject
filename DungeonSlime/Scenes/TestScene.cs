419bc9c9f7a98e5922136b99376d2c5e12df4a1a
Rioni <nrosa01@ucm.es>
2026-07-06 21:57:27 +0200

    Trying the ECS
    Now I have to add debug features, performance measures and make it better

--- DungeonSlime/Scenes/TestScene.cs
+++ DungeonSlime/Scenes/TestScene.cs
@@ -19,7 +19,10 @@ namespace DungeonSlime.Scenes
         AnimatedSprite slimeAnimation;
         Vector2 position;
         Vector2 velocity;
+        World world;
         private float speed = 500f;
+
+        record struct Square(int Size);
 
         record struct Position(float X, float Y);
         record struct Velocity(float X, float Y);
@@ -35,44 +38,52 @@ namespace DungeonSlime.Scenes
             slimeAnimation = atlas.CreateAnimatedSprite("slime-animation");
             slimeAnimation.Scale = new Vector2(4.0f, 4.0f);
     
-            var world = new World(10);
+            world = new World(5000);
 
-            for (var i = 0; i < 3; i++)
+            for (var i = 0; i < 3000; i++)
             {
+                var spacing = .2f;
                 var entity = world.Create();
-                world.AddComponent(entity, new Position { X = (i + 1) * 10, Y = (i + 1) * 10 });
-                world.AddComponent(entity, new Velocity { X = 2, Y = 2 });
+                world.AddComponent(entity, new Position { X = (i + 1) * spacing, Y = (i + 1) * spacing });
+                world.AddComponent(entity, new Velocity { X = 8 + i * 0.01f, Y = 4f + i * 0.01f });
+                world.AddComponent(entity, new Square { Size = 10 });
 
                 if (i % 2 == 0) world.AddComponent(entity, new Fart { Power = 666 });
             }
-
-            RunPrinterSystem(world);
-            RunVelocitySystem(world);
-
-            RunPrinterSystem(world);
-            RunVelocitySystem(world);
         }
 
         static void RunVelocitySystem(World registry)
         {
-            var view = registry.View<Velocity, Position>();
+            var view = registry.View<Velocity, Position, Square>();
+            var WindowsWidth = Core.GraphicsDevice.Viewport.Width;
+            var WindowsHeight = Core.GraphicsDevice.Viewport.Height;
             foreach (var entity in view)
             {
                 ref Position pos = ref registry.GetComponent<Position>(entity);
                 ref Velocity vel = ref registry.GetComponent<Velocity>(entity);
+                ref Square square = ref registry.GetComponent<Square>(entity);
                 pos.X += vel.X;
                 pos.Y += vel.Y;
+
+                if (pos.X < 0 || pos.X + square.Size > WindowsWidth)
+                    vel.X *= -1;
+
+                if (pos.Y < 0 || pos.Y + square.Size > WindowsHeight)
+                    vel.Y *= -1;
+
             }
         }
 
-        static void RunPrinterSystem(World registry)
+        static void RunSquareSystem(World registry, SpriteBatch spritebatch)
         {
             Console.WriteLine("----- Printer -----");
-            var view = registry.View<Velocity, Position, Fart>();
+            var view = registry.View<Position, Square>();
             foreach (var entity in view)
             {
                 var pos = registry.GetComponent<Position>(entity);
-                Console.WriteLine($"entity: {entity}, pos: {pos.X},{pos.Y}");
+                var square = registry.GetComponent<Square>(entity);
+
+                spritebatch.Draw(Core.Pixel, new Rectangle((int)pos.X, (int)pos.Y, square.Size, square.Size), null, Color.Wheat);
             }
         }
 
@@ -87,6 +98,7 @@ namespace DungeonSlime.Scenes
             base.Update(gameTime);
             slimeAnimation.Update(gameTime);
             HandleInput(gameTime);
+            RunVelocitySystem(world);
         }
 
         private void HandleInput(GameTime gameTime)
@@ -115,6 +127,7 @@ namespace DungeonSlime.Scenes
 
             Core.SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
             slimeAnimation.Draw(Core.SpriteBatch, position);
+            RunSquareSystem(world, Core.SpriteBatch);
             Core.SpriteBatch.End();
 
             Core.ImGuiRenderer.BeforeLayout(gameTime);
