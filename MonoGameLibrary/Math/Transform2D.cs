cb9d98ec8cb68f3d549d7eb338032386eeebe7f6
Rioni <nrosa01@ucm.es>
2026-07-14 22:12:52 +0200

    Create Transform2D class, not implemented yet

---
+++ MonoGameLibrary/Math/Transform2D.cs
@@ -0,0 +1,17 @@
+﻿// Darkrit - Copyright (C) Nicolás Rosa (@nrosa01)
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Darkrit.Math
+{
+    public struct Transform2D
+    {
+        // Affin transform
+        Vector2 BasisX, BasisY, Translation;
+    }
+}
