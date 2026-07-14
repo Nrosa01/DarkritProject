872d0ff9b3b50619ab1baa92eb75a1e1fc696e50
Rioni <nrosa01@ucm.es>
2026-07-14 20:08:10 +0200

    Component implementation

--- MonoGameLibrary/EntityModel/Component.cs
+++ MonoGameLibrary/EntityModel/Component.cs
@@ -2,27 +2,16 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE.txt', which is part of this source code package.
 
-using System;
-using System.Collections.Generic;
-using System.Text;
+// The following code was adapted from: https://github.com/BobbyAnguelov/Esoterica/blob/main/Code/Engine/Entity/EntityComponent.h
+
+using System.Diagnostics;
 using Darkrit.Base;
+using Microsoft.Xna.Framework.Content;
 
 namespace Darkrit.EntityModel
 {
-    public class Component
+    public abstract class Component
     {
-        protected ComponentID componentID = ComponentID.Generate();
-
-        public EntityID EntityID { get;  protected set; }
-
-        public readonly StringID NameID;
-
-        protected Status status = Status.Unloaded;
-
-        protected bool isRegisteredWithEntity = false;
-
-        protected bool isRegisteredWithWorld = false;
-
         public enum Status
         {
             Unloaded = 0,
@@ -32,5 +21,60 @@ namespace Darkrit.EntityModel
             Initialized
         }
 
+        // The unique ID for this component
+        protected ComponentID componentID = ComponentID.Generate();
+
+        // The ID of the entity that owns this component
+        public EntityID EntityID { get;  protected set; }
+
+        // The name of the component
+        public StringID NameID { get; protected set; }
+
+        // Component status
+        public Status CurrentStatus { get; protected set; } = Status.Unloaded;
+
+        public bool HasLoadingFailed => CurrentStatus == Status.LoadingFailed;
+        public bool IsUnloaded => CurrentStatus == Status.Unloaded;
+        public bool IsLoading => CurrentStatus == Status.Loading;
+        public bool IsLoaded => CurrentStatus == Status.Loaded;
+        public bool IsInitialized => CurrentStatus == Status.Initialized;
+
+
+        // Registered with its parent entity's local systems
+        protected bool isRegisteredWithEntity = false;
+        // Registered with the global systems in it's parent world
+        protected bool isRegisteredWithWorld = false;
+
+        // Do we allow multiple components of the same type per entity?
+        public virtual bool IsSingletonComponent => true;
+
+        // Request load of all component data - loading takes time
+        protected abstract void Load(ContentManager contentManager);
+
+        // Update loading state, this will check all dependencies
+        protected abstract void UpdateLoading();
+
+        // Request unload of component data, unloading is instant
+        protected abstract void Unload(ContentManager contentManager);
+
+        // Called when an component finishes loading all its resources
+        // Note: this is only called if the loading succeeds and you are guaranteed all resources to be valid and so should assert on that
+        internal void InternalInitialize()
+        {
+            Debug.Assert(EntityID.IsValid && CurrentStatus == Status.Loaded);
+            CurrentStatus = Status.Initialized;
+            Initialize();
+        }
+
+        protected abstract void Initialize();
+
+        // Called just before a component begins unloading
+        internal void InternalShutdown()
+        {
+            Debug.Assert(EntityID.IsValid && CurrentStatus == Status.Initialized);
+            Shutdown();
+        }
+
+        protected abstract void Shutdown();
     }
 }
