9fc120992b4a9cf926e45dd51c358b8769823c72
Rioni <nrosa01@ucm.es>
2026-08-16 23:49:55 +0200

    Add Code generator ref to Darkrit

---
+++ Darkrit/EntityModel/Components/SpriteRendererComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Darkrit.EntityModel.Components;
+
+[Component]
+public partial struct SpriteRendererComponent
+{
+    public void Draw(GameTime gameTime)
+    {
+        var t = Entity.Transform;
+    }
+}
