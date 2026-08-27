fd3aa28a4126ce4b0285cc9ba63e876345914608
Rioni <nrosa01@ucm.es>
2026-08-28 01:22:28 +0200

    Temporarily disable teleport on moving platformo

--- Game/Components/MovingPlatform.cs
+++ Game/Components/MovingPlatform.cs
@@ -28,7 +28,5 @@ public partial struct MovingPlatform
         {
             X = startPosition.X + MathF.Sin(timer * speed) * amplitude
         };
-
-        PhysicsBody.Teleport(Entity.Position);
     }
 }
